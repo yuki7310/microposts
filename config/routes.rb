fc9e92983646d471004ebd756e733810e7e36c0a
Yuki <ykyk070310@gmail.com>
2019-08-05 12:05:52 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get '/help' => 'static_pages#help'
+  get '/about' => 'static_pages#about'
+  get '/contact' => 'static_pages#contact'
+  get '/signup' => 'users#new'
 
 end
