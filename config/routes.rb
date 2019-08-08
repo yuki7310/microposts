88700b811487abdcc2e3ab643293565f97bd05a8
Yuki <ykyk070310@gmail.com>
2019-08-08 07:07:44 +0000

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get '/contact' => 'static_pages#contact'
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
   resources :users
 
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -26,5 +26,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_template "users/show"
     assert_not flash.empty?
+    assert logged_in?
   end
 end
